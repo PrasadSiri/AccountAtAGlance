716520148a88b0052c19db5a4c3d579926373ce7
Anson Smith <AnsonSmith@quickenloans.com>
2012-04-23 15:44:42 -0400

    Initial Commit

---
+++ src/AccountAtAGlance.Repository/AccountAtAGlance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using AccountAtAGlance.Model;
+
+namespace AccountAtAGlance.Repository
+{
+    public class AccountAtAGlance : DbContext, IDisposedTracker
+    {
+        public DbSet<BrokerageAccount> BrokerageAccounts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Exchange> Exchanges { get; set; }
+        public DbSet<MarketIndex> MarketIndexes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderType> OrderTypes { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Security> Securities { get; set; }
+        public DbSet<MutualFund> MutualFunds { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<WatchList> WatchLists { get; set; }
+        public bool IsDisposed { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Security>().ToTable("Securities");
+            modelBuilder.Entity<Stock>().ToTable("Securities_Stock");
+            modelBuilder.Entity<MutualFund>().ToTable("Securities_MutualFund");
+
+            modelBuilder.Entity<WatchList>()
+                    .HasMany(w => w.Securities).WithMany()
+                    .Map(map => map.ToTable("WatchListSecurity")
+                    .MapRightKey("SecurityId")
+                    .MapLeftKey("WatchListId"));
+
+        }
+
+        public int DeleteAccounts()
+        {
+            return base.Database.ExecuteSqlCommand("DeleteAccounts");
+        }
+        public int DeleteSecuritiesAndExchanges()
+        {
+            return base.Database.ExecuteSqlCommand("DeleteSecuritiesAndExchanges");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            IsDisposed = true;
+            base.Dispose(disposing);
+        }
+    }
+
+   
+}
---
+++ src/AccountAtAGlance.Repository/ModelContainer.cs
@@ -0,0 +1,37 @@
+﻿using AccountAtAGlance.Repository;
+using Microsoft.Practices.Unity;
+using AccountAtAGlance.Repository;
+
+namespace AccountAtAGlance.Model
+{
+    //This is a simplified version of the code shown in the videos
+    //The instance of UnityContainer is created in the constructor 
+    //rather than checking in the Instance property and performing a lock if needed
+    public static class ModelContainer
+    {
+        private static readonly object lockObject = new object();
+        private static IUnityContainer _Instance;
+
+        
+        public static IUnityContainer Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (_Instance == null)
+                        {
+                            _Instance = new UnityContainer();
+                            _Instance.RegisterType<IAccountRepository, AccountRepository>(new HierarchicalLifetimeManager());
+                            _Instance.RegisterType<ISecurityRepository, SecurityRepository>(new HierarchicalLifetimeManager());
+                            _Instance.RegisterType<IMarketsAndNewsRepository, MarketsAndNewsRepository>(new HierarchicalLifetimeManager());
+                        }
+                    }
+                }
+                return _Instance;
+            }
+        }
+    }
+}
